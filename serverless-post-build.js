cd1c27b0f18c3ac9da2a03f77f5edafd6fd89b88
Paweł Lorenc <pawel.lorenc@rst-it.com>
2021-06-23 07:34:32 +0200

    fix: try again with post build script

--- serverless-post-build.js
+++ serverless-post-build.js
@@ -3,9 +3,9 @@
 const fs = require("fs-extra");
 console.log("-> Copying locales directory...");
 // Path to locales directory
-const localeSrc = "./static/locales";
+const localeSrc = "./public/locales";
 // Path to default-lambda destination
-const localeDest = "./.serverless_nextjs/default-lambda/static/locales";
+const localeDest = "./.serverless_nextjs/default-lambda/public/locales";
 // Copy Files over recursively
 fs.copySync(localeSrc, localeDest, { recursive: true });
 console.log("Locale directory was copied successfully");
