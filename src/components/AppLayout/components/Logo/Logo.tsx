5bce8badf3bebeb1877bcf91852619b5319ae14a
Kacper Szewczyk <mistrzszefow@gmail.com>
2021-06-23 08:14:13 +0200

    Fix logo and fav icon

--- src/components/AppLayout/components/Logo/Logo.tsx
+++ src/components/AppLayout/components/Logo/Logo.tsx
@@ -8,7 +8,7 @@ const Logo: FC<LogoProps> = ({ title }) => (
   <>
     <div className="flex items-left w-full py-4 container m-auto">
       <img src="/images/logo-sb.svg" alt="logo-sb" className="h-12 md:h-6 px-1 md:px-3"/>
-      <img src="/images/logo-cw.jpg" alt="logo-cw" className="h-12 md:h-6 px-1 md:px-3"/>
+      <img src="/images/logo-cw.png" alt="logo-cw" className="h-12 md:h-6 px-1 md:px-3"/>
       <div className="minh-full md:hidden border-red-900 border-solid border-l-2 ml-4">
       </div>
       <div className="md:flex">
